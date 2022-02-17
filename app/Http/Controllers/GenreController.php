3a21835bf30e0cffa9254607e1ab504003e291b7
PrinceNiyonshuti <npprince47@gmail.com>
2022-02-17 14:19:31 +0200

    chore(Migration and controller): made migration and controllers for movies for genres

---
+++ app/Http/Controllers/GenreController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Genre;
+use Illuminate\Http\Request;
+
+class GenreController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Genre  $genre
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Genre $genre)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Genre  $genre
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Genre $genre)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Genre  $genre
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Genre $genre)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Genre  $genre
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Genre $genre)
+    {
+        //
+    }
+}
--- database/migrations/2022_02_17_121733_create_genres_table.php
+++ database/migrations/2022_02_17_121733_create_genres_table.php
@@ -13,15 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('genres', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
-            $table->string('firstName');
-            $table->string('lastName');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -33,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('genres');
     }
 };
