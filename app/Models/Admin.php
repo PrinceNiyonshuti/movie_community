df534b8dacd2892490d342b1ebd33713c3a49be1
PrinceNiyonshuti <npprince47@gmail.com>
2022-02-27 12:36:18 +0200

    added admin functonality

---
+++ app/Models/Admin.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Admin extends Authenticatable
+{
+    use HasFactory;
+}
