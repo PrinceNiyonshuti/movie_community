4b751340dea2f0bf9e2a309a1194eca951c639ba
PrinceNiyonshuti <npprince47@gmail.com>
2022-06-12 19:28:03 +0200

    ft(Middleware): updated IsAdmin functionality

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -17,7 +17,7 @@ class IsAdmin
      */
     public function handle(Request $request, Closure $next)
     {
-        if (auth()->user()?->level_id != '3') {
+        if (auth()->user()?->IsAdmin != '1') {
             abort(Response::HTTP_FORBIDDEN);
         }
     }
