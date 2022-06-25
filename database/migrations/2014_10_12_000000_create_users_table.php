9eddf1a13d3c4102909738b1b072e9d9043ec509
PrinceNiyonshuti <npprince47@gmail.com>
2022-06-25 11:09:09 +0200

    fx(User): added avatar aattribute to user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('firstName')->nullable();
             $table->string('lastName')->nullable();
             $table->string('email')->unique();
+            $table->string('avatar')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
