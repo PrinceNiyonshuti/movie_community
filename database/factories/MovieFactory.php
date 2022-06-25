77c5ca086472c9b92372b439a1c2a5f674a036ee
PrinceNiyonshuti <npprince47@gmail.com>
2022-06-25 11:36:18 +0200

    ft(Factory): updated movie factory data

--- database/factories/MovieFactory.php
+++ database/factories/MovieFactory.php
@@ -23,7 +23,7 @@ class MovieFactory extends Factory
             'user_id' => User::all()->random()->id,
             'genre_id' => Genre::all()->random()->id,
             'name' => $this->faker->firstName(),
-            'description' => $this->faker->sentence(5),
+            'description' => $this->faker->sentence(20),
             'released_date' => $this->faker->date(),
             'director' => $this->faker->unique()->name(),
             'writer' => $this->faker->unique()->name(),
