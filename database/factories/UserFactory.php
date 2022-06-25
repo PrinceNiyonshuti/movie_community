2b5ded4a40755bd5f2544591f789749fcb3a59e4
PrinceNiyonshuti <npprince47@gmail.com>
2022-06-25 11:27:59 +0200

    ft(Factory): added user factory data

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,8 +18,12 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
+            'username' => $this->faker->unique()->userName(),
+            'firstName' => $this->faker->unique()->firstName(),
+            'lastName' => $this->faker->unique()->lastName(),
             'email' => $this->faker->unique()->safeEmail(),
+            'avatar' => $this->faker->imageUrl($width = 200, $height = 200),
+            'IsAdmin' => $this->faker->boolean(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
