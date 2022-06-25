e2ee0a58a09a1c52fd353e16a14191893ead9f10
PrinceNiyonshuti <npprince47@gmail.com>
2022-06-25 11:09:39 +0200

    fx(User): added Is_admin attribute to user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->string('lastName')->nullable();
             $table->string('email')->unique();
             $table->string('avatar')->nullable();
+            $table->boolean('IsAdmin')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
