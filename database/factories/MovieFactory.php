1e43d8859dfeb5636dd14290e985bd78a48ea0bb
PrinceNiyonshuti <npprince47@gmail.com>
2022-07-18 16:10:19 +0200

    ch(Movie): added back the defualt movie faker

--- database/factories/MovieFactory.php
+++ database/factories/MovieFactory.php
@@ -27,8 +27,7 @@ class MovieFactory extends Factory
             'released_date' => $this->faker->date(),
             'director' => $this->faker->unique()->name(),
             'writer' => $this->faker->unique()->name(),
-            'thumbnail' => 'https://cdn.shopify.com/s/files/1/0057/3728/3618/products/black-adam_t9tj2u28_480x.progressive.jpg?v=1657031326',
-            // 'thumbnail' => $this->faker->imageUrl($width = 200, $height = 200),
+            'thumbnail' => $this->faker->imageUrl($width = 200, $height = 200),
             'video' => $this->faker->imageUrl($width = 200, $height = 200),
         ];
     }
