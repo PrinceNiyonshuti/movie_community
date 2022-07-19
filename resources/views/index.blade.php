9d5a6be77f6c3696d332b075efdb1cf99e386a17
PrinceNiyonshuti <npprince47@gmail.com>
2022-07-19 16:09:33 +0200

    rf(Movie): changed latest default movie thumbnail

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -41,7 +41,7 @@
                     <div class="flex-wrap-movielist grid-fav">
                         @foreach ($movies as $movie)
                         <div class="movie-item-style-2 movie-item-style-1 style-3">
-                            <img src="{{ $movie->thumbnail? '/storage/$movie->thumbnail':'https://cdn.shopify.com/s/files/1/0057/3728/3618/products/black-adam_t9tj2u28_480x.progressive.jpg?v=1657031326' }}" width="160" height="224" alt="{{ $movie->name}}">
+                            <img src="{{ $movie->thumbnail? '/storage/$movie->thumbnail':asset('images/uploads/mv-it4.jpg') }}" width="160" height="224" alt="{{ $movie->name}}">
                             <div class="hvr-inner">
                                 <a href="/movies/{{ $movie->name}}"> Read more <i class="ion-android-arrow-dropright"></i> </a>
                             </div>
