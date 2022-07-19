69fe01213d47155dcf1b6f6dd5f1fdc4672551b8
PrinceNiyonshuti <npprince47@gmail.com>
2022-07-19 16:02:00 +0200

    rf(users): changed user avatar factory to be null

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,7 +22,7 @@ class UserFactory extends Factory
             'firstName' => $this->faker->unique()->firstName(),
             'lastName' => $this->faker->unique()->lastName(),
             'email' => $this->faker->unique()->safeEmail(),
-            'avatar' => $this->faker->imageUrl($width = 200, $height = 200),
+            // 'avatar' => $this->faker->imageUrl($width = 200, $height = 200),
             'IsAdmin' => $this->faker->numberBetween($min = 0, $max = 1),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
