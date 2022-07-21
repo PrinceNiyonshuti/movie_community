6a6c878403941217d9580585adc01de882f4f49c
PrinceNiyonshuti <npprince47@gmail.com>
2022-07-21 19:00:24 +0200

    ft(favorite): added user foreign ID

--- database/migrations/2022_07_21_165840_create_favorites_table.php
+++ database/migrations/2022_07_21_165840_create_favorites_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('favorites', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id');
             $table->timestamps();
         });
     }
