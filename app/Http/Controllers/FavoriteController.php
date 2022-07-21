99fd1e05b0ce96af1260a1028c03247f27ced197
PrinceNiyonshuti <npprince47@gmail.com>
2022-07-21 18:59:51 +0200

    ft(favorite): created model , migration , factory and controller

---
+++ app/Http/Controllers/FavoriteController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\favorite;
+use Illuminate\Http\Request;
+
+class FavoriteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\favorite  $favorite
+     * @return \Illuminate\Http\Response
+     */
+    public function show(favorite $favorite)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\favorite  $favorite
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(favorite $favorite)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\favorite  $favorite
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, favorite $favorite)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\favorite  $favorite
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(favorite $favorite)
+    {
+        //
+    }
+}
---
+++ database/factories/FavoriteFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\favorite>
+ */
+class FavoriteFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
