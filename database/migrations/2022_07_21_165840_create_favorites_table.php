44e8fbb3bebd1c636cdd0e5832b1139a6b2117e0
PrinceNiyonshuti <npprince47@gmail.com>
2022-07-21 19:01:45 +0200

    ft(favorite): added counter column

--- database/migrations/2022_07_21_165840_create_favorites_table.php
+++ database/migrations/2022_07_21_165840_create_favorites_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id');
             $table->foreignId('movie_id');
+            $table->integer('counter');
             $table->timestamps();
         });
     }
