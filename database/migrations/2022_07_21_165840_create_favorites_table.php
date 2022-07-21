e82eb6ce4309a960ccd0c4504284dac5034058c5
PrinceNiyonshuti <npprince47@gmail.com>
2022-07-21 19:00:39 +0200

    ft(favorite): added movie foreign ID

--- database/migrations/2022_07_21_165840_create_favorites_table.php
+++ database/migrations/2022_07_21_165840_create_favorites_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('favorites', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id');
+            $table->foreignId('movie_id');
             $table->timestamps();
         });
     }
