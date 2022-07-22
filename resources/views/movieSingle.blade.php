c3fa9d4b012d224ab3a95becf6ecd47c1501452b
PrinceNiyonshuti <npprince47@gmail.com>
2022-07-22 11:47:13 +0200

    fx(Movie):fixed dummy data in single movie view

--- resources/views/movieSingle.blade.php
+++ resources/views/movieSingle.blade.php
@@ -77,34 +77,6 @@
                                                 </div>
                                                 <a href="#" class="redbtn">Write Review</a>
                                             </div>
-                                            <div class="topbar-filter">
-                                                <p>Found <span>56 reviews</span> in total</p>
-
-                                            </div>
-                                            <div class="mv-user-review-item">
-                                                <div class="user-infor">
-                                                    <img src="{{ asset('images/uploads/userava1.jpg') }}" alt="">
-                                                    <div>
-                                                        <h3>Best Marvel movie in my opinion</h3>
-                                                        <div class="no-star">
-                                                            <i class="ion-android-star"></i>
-                                                            <i class="ion-android-star"></i>
-                                                            <i class="ion-android-star"></i>
-                                                            <i class="ion-android-star"></i>
-                                                            <i class="ion-android-star"></i>
-                                                            <i class="ion-android-star"></i>
-                                                            <i class="ion-android-star"></i>
-                                                            <i class="ion-android-star"></i>
-                                                            <i class="ion-android-star"></i>
-                                                            <i class="ion-android-star last"></i>
-                                                        </div>
-                                                        <p class="time">
-                                                            17 December 2016 by <a href="#"> hawaiipierson</a>
-                                                        </p>
-                                                    </div>
-                                                </div>
-                                                <p>This is by far one of my favorite movies from the MCU. The introduction of new Characters both good and bad also makes the movie more exciting. giving the characters more of a back story can also help audiences relate more to different characters better, and it connects a bond between the audience and actors or characters. Having seen the movie three times does not bother me here as it is as thrilling and exciting every time I am watching it. In other words, the movie is by far better than previous movies (and I do love everything Marvel), the plotting is splendid (they really do out do themselves in each film, there are no problems watching it more than once.</p>
-                                            </div>
                                         </div>
                                     </div>
                                     <div id="moviesrelated" class="tab">
