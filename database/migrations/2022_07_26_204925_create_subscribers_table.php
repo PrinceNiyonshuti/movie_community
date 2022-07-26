8970074e9c5e8a39095b5070532b59cb4e375a20
PrinceNiyonshuti <npprince47@gmail.com>
2022-07-26 22:51:46 +0200

    ft(Subscriber): added model attributes

--- database/migrations/2022_07_26_204925_create_subscribers_table.php
+++ database/migrations/2022_07_26_204925_create_subscribers_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('subscribers', function (Blueprint $table) {
             $table->id();
+            $table->text('email')->unique();
             $table->timestamps();
         });
     }
