d6a06a9345f64a2cd11e4ff9234905960df3adb6
PrinceNiyonshuti <npprince47@gmail.com>
2022-08-08 12:19:56 +0200

    ft(members): added routes for members

--- routes/web.php
+++ routes/web.php
@@ -68,3 +68,7 @@ Route::post('/unFavorite/{movie:name}', [FavoriteController::class, 'destroy']);
 // Subscriber routes
 
 Route::post('/subscribe', [SubscriberController::class, 'store']);
+
+// Members routes
+
+Route::post('/members', [GuestController::class, 'list']);
