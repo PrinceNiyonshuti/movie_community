c01bc8452c46765c3bfd3bf303a0292469d58efb
PrinceNiyonshuti <npprince47@gmail.com>
2022-08-13 11:40:30 +0200

    (rf-avatar): added main layout header to favorites view

--- resources/views/account/favorite.blade.php
+++ resources/views/account/favorite.blade.php
@@ -1,3 +1,17 @@
 <x-layout>
-    
+    <div class="hero user-hero">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="hero-ct">
+                        <x-profile-name />
+                        <ul class="breadcumb">
+                            <li class="active"><a href="/">Home</a></li>
+                            <li> <span class="ion-ios-arrow-right"></span>Dashboard</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-layout>
