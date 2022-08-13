6dfb9ed20ea30cba8d1384308bcade14ba437829
PrinceNiyonshuti <npprince47@gmail.com>
2022-08-13 11:52:01 +0200

    (rf-avatar): added favorite movies from controller

--- resources/views/account/favorite.blade.php
+++ resources/views/account/favorite.blade.php
@@ -21,8 +21,20 @@
                 <div class="col-md-9 col-sm-12 col-xs-12">
                     <div class="topbar-filter user">
                         <p>Found <span>{{ $movies->count() }} movies</span> in total</p>
-                        <div class="flex-wrap-movielist grid-fav">
+                    </div>
+                    <div class="flex-wrap-movielist grid-fav">
+                        @foreach ($movies as $movie)
+                        <div class="movie-item-style-2 movie-item-style-1 style-3">
+                            <img src="{{ $movie->thumbnail? '/storage/$movie->thumbnail':asset('images/uploads/mv-it6.jpg') }}" alt="">
+                            <div class="hvr-inner">
+                                <a href="/movies/{{ $movie->name}}"> Read more <i class="ion-android-arrow-dropright"></i> </a>
+                            </div>
+                            <div class="mv-item-infor">
+                                <h6><a href="/movies/{{ $movie->name}}">{{ $movie->name }}</a></h6>
+                                <p class="yellow"><a href="/category/{{ $movie->genre->name }}">{{ $movie->genre->name }}</a></p>
+                            </div>
                         </div>
+                        @endforeach
                     </div>
                 </div>
             </div>
