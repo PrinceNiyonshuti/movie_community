b8ba37f4c4586520ce4771af15a0fcd95fd4c2d3
PrinceNiyonshuti <npprince47@gmail.com>
2022-09-02 23:11:25 +0200

    (rf-profile): refactored checking the existing user before update and updating function

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -20,14 +20,14 @@ class DashboardController extends Controller
     }
     public function update(Request $request, $id)
     {
-        if ($existingMember) {
-            $existingMember->username = $request['username'];
-            $existingMember->firstName = $request['firstName'];
-            $existingMember->lastName = $request['lastName'];
+        if (auth()->user()) {
+            auth()->user()->username = $request['username'];
+            auth()->user()->firstName = $request['firstName'];
+            auth()->user()->lastName = $request['lastName'];
             if (isset($request['avatar'])) {
-                $existingMember->avatar = $request->file('avatar')->store('avatars');
+                auth()->user()->avatar = $request->file('avatar')->store('avatars');
             }
-            $existingMember->save();
+            auth()->user()->save();
         }
         return redirect('/account/profile')->with('success', 'Profile updated successfully!');
     }
