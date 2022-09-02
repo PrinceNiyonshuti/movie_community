81807ce51ec968da35567f4fc3ef42f7ffc84cb2
PrinceNiyonshuti <npprince47@gmail.com>
2022-09-02 23:09:36 +0200

    (rf-profile): updated account update route data

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,7 @@ Route::post('/logout', [AuthController::class, 'destroy'])->middleware('auth');
 Route::middleware('auth')->prefix('/account')->group(function () {
     Route::get('', [DashboardController::class, 'index']);
     Route::get('/profile', [DashboardController::class, 'create']);
-    Route::post('/{movie}', [DashboardController::class, 'update']);
+    Route::post('/{user}', [DashboardController::class, 'update']);
 });
 
 // Movie actions
