72cad3e0b7b13e37c515f794e4a17d15c2715937
PrinceNiyonshuti <npprince47@gmail.com>
2022-09-04 21:34:48 +0200

    (resources-page): added header to terms of use

--- resources/views/pages/terms.blade.php
+++ resources/views/pages/terms.blade.php
@@ -1,3 +1,17 @@
 <x-layout>
-    
+    <div class="hero common-hero">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="hero-ct">
+                        <h1>Terms of use</h1>
+                        <ul class="breadcumb">
+                            <li class="active"><a href="/">Home</a></li>
+                            <li> <span class="ion-ios-arrow-right"></span>Terms of use</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-layout>
