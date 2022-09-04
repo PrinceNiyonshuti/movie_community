9b4d625a80ec50945dd60b9b7e9d3f095e9868a0
PrinceNiyonshuti <npprince47@gmail.com>
2022-09-04 21:32:50 +0200

    (resources-page): added header to contact page

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,2 +1,17 @@
 <x-layout>
+    <div class="hero common-hero">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="hero-ct">
+                        <h1> Contact</h1>
+                        <ul class="breadcumb">
+                            <li class="active"><a href="/">Home</a></li>
+                            <li> <span class="ion-ios-arrow-right"></span> reach to us</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-layout>
