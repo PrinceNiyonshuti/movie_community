2fe0594323081cc2d2ba5b1f55e08c7d412c1924
PrinceNiyonshuti <npprince47@gmail.com>
2022-09-11 17:33:10 +0200

    (rf-member-ui): added the search area

--- resources/views/members.blade.php
+++ resources/views/members.blade.php
@@ -67,43 +67,12 @@
                 <div class="col-md-3 col-xs-12 col-sm-12">
                     <div class="sidebar">
                         <div class="searh-form">
-                            <h4 class="sb-title">Search celebrity</h4>
-                            <form class="form-style-1 celebrity-form" action="#">
+                            <h4 class="sb-title">Search Member</h4>
+                            <form class="form-style-1 Member-form" action="#">
                                 <div class="row">
                                     <div class="col-md-12 form-it">
-                                        <label>Celebrity name</label>
+                                        <label>Member name</label>
                                         <input type="text" placeholder="Enter keywords">
-                                    </div>
-                                    <div class="col-md-12 form-it">
-                                        <label>Celebrity Letter</label>
-                                        <select>
-                                            <option value="range">A</option>
-                                            <option value="saab">B</option>
-                                        </select>
-                                    </div>
-                                    <div class="col-md-12 form-it">
-                                        <label>Category</label>
-                                        <select>
-                                            <option value="range">Actress</option>
-                                            <option value="saab">Others</option>
-                                        </select>
-                                    </div>
-                                    <div class="col-md-12 form-it">
-                                        <label>Year of birth</label>
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <select>
-                                                    <option value="range">1970</option>
-                                                    <option value="number">Other</option>
-                                                </select>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <select>
-                                                    <option value="range">1990</option>
-                                                    <option value="number">others</option>
-                                                </select>
-                                            </div>
-                                        </div>
                                     </div>
                                     <div class="col-md-12 ">
                                         <input class="submit" type="submit" value="submit">
