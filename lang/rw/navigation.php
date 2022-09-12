8ef2bf64a33bf2f73706b3ef63fc181b1e06fdf1
PrinceNiyonshuti <npprince47@gmail.com>
2022-09-12 13:20:32 +0200

    (ft-localization): added kinyarwanda language

---
+++ lang/rw/navigation.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'home'          => 'Ahabanza',
+    'all_movies'  => 'Film zose',
+    'search_home'       => 'Search for a movie , Tv Show or celebrity that you are looking for',
+    'login'         => 'Login',
+    'register'         => 'Register',
+];
