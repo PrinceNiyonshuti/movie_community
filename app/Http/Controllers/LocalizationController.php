4ab2c897a20d87cdef78302b486ccd95abc25c8c
PrinceNiyonshuti <npprince47@gmail.com>
2022-09-12 12:56:32 +0200

    (ft-localization): added local settings to  localization controller

--- app/Http/Controllers/LocalizationController.php
+++ app/Http/Controllers/LocalizationController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Session;
 
 class LocalizationController extends Controller
 {
-    //
+    public function index($locale)
+    {
+        if (array_key_exists($locale, Config::get('languages'))) {
+            Session::put('applocale', $locale);
+        }
+
+        return redirect()->back();
+    }
 }
