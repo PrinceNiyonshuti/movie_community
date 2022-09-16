ea2f1a89ac0c55e44399a90268b69c9c482734c2
PrinceNiyonshuti <npprince47@gmail.com>
2022-09-16 18:02:21 +0200

    (ft-localizatio-extending): added basic movie translation file for kinyarwanda

---
+++ lang/rw/movie.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'movie_list' => 'Urutonde rwa filmi',
+    'list' => 'Urutonde',
+    'found' => 'Habonetse',
+    'in_total' => 'Muri rusange',
+    'search_for_movie' => 'Shakisha filmi',
+];
