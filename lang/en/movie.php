f8bf3767b8ad14851da58b310698771fedba1274
PrinceNiyonshuti <npprince47@gmail.com>
2022-09-16 18:00:36 +0200

    (ft-localizatio-extending): added basic movie translation file for english

---
+++ lang/en/movie.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'movie_list' => 'Movie List',
+    'list' => 'List',
+    'found' => 'Found',
+    'in_total' => 'in total',
+    'search_for_movie' => 'Search for movie',
+];
