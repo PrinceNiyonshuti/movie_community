71d81e4aa9e80ebb0019550377fdf0ed6804de1d
PrinceNiyonshuti <npprince47@gmail.com>
2022-10-17 10:26:35 +0200

    (ch-member-translation): updated kinyarwanda member file

--- lang/rw/member.php
+++ lang/rw/member.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'member_list' => 'Urutonde rwa filmi',
+    'member_list' => 'Urutonde rwa abanyamuryango',
     'list' => 'Urutonde',
     'found' => 'Habonetse',
     'in_total' => 'Muri rusange',
-    'search_for_member' => 'Shakisha filmi',
-    'member_name' => 'Izina ry filmi',
+    'search_for_member' => 'Shakisha abanyamuryango',
+    'member_name' => 'Izina ry abanyamuryango',
     'search' => 'Shakisha',
 ];
