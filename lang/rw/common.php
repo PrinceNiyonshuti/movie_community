1f045b5f2797ff9f25c915e86d5fe97590132070
PrinceNiyonshuti <npprince47@gmail.com>
2022-11-30 14:38:05 +0200

    (rf-pages): added common translation for english

---
+++ lang/rw/common.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'contact_us' => 'Contact Us',
+    'blog' => 'Blog',
+    'term_of_us' => 'Terms of Use',
+    'privacy_policy' => 'Privacy Policy',
+    'security' => 'Security',
+    'my_account' => 'My Account',
+    'watch_list' => 'Watch list',
+];
