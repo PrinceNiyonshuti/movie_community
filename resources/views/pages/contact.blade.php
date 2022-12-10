fc3129711e4cc4f92beecf433fab1a49605c1f9b
PrinceNiyonshuti <npprince47@gmail.com>
2022-12-10 10:21:06 +0200

    (ft-contact): removed commented codes

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -17,29 +17,6 @@
     <div class="page-single">
         <div class="container">
             <div class="row ipad-width">
-                <!-- <div class="col-md-3 col-sm-12 col-xs-12">
-                    <div class="user-information">
-                        <div class="user-img">
-                            <a href="#"><img src="images/uploads/user-img.png" alt=""><br></a>
-                            <a href="#" class="redbtn">Change avatar</a>
-                        </div>
-                        <div class="user-fav">
-                            <p>Account Details</p>
-                            <ul>
-                                <li class="active"><a href="userprofile.html">Profile</a></li>
-                                <li><a href="userfavoritelist.html">Favorite movies</a></li>
-                                <li><a href="userrate.html">Rated movies</a></li>
-                            </ul>
-                        </div>
-                        <div class="user-fav">
-                            <p>Others</p>
-                            <ul>
-                                <li><a href="#">Change password</a></li>
-                                <li><a href="#">Log out</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div> -->
                 <div class="col-md-12 col-sm-12 col-xs-12">
                     <div class="form-style-1 user-pro" action="#">
                         <form action="#" class="user">
