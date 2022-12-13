ef4da61f0a723739a210b0ec42602e2652e57f22
PrinceNiyonshuti <npprince47@gmail.com>
2022-12-13 08:48:36 +0200

    (ft-terms): added Use of the Resources section

--- resources/views/pages/terms.blade.php
+++ resources/views/pages/terms.blade.php
@@ -32,6 +32,16 @@
                     <p>You are welcome to submit any material for our consideration. We should warn you, however, that we do not undertake to keep such material confidential, and we have the absolute right to make use of it in any way we choose without incurring any liability to you, whether in respect of payment or otherwise.</p>
                     <p>If we do actively seek a submission, you must ensure that your submissions comply with all relevant laws and you are advised to seek legal advice in advance. If our use of your submission results in any liability to us, you agree to make good all losses we incur of whatever nature.</p>
                 </div>
+                <div class="block block-text">
+                    <h2>Use of the Resources</h2>
+                    <p>Content on the Into Film website ("Resources") may only be copied, downloaded and/or redistributed if that use of the Resource is not for a commercial purpose and falls within a copyright exception provided for in the Copyright, Designs and Patents Act 1988, including one or more the following:</p>
+                    <p><strong>(a) </strong><strong>Fair dealing for the purposes of private study (Section 29)</strong></p>
+                    <p>Unless your copying of the content is for your own study purposes, this exception is unlikely to apply if multiple copies are made for more than one person to use for the same purpose, e.g. hand-outs for a class of students.</p>
+                    <p><strong>(b) Fair dealing for the sole and non-commercial purpose of illustration for instruction (Section 32)</strong></p>
+                    <p>If you are a teacher, pupil or parent of a pupil, you may be allowed to copy content in the course of teaching or being taught, or of preparing for teaching for being taught. You will need to give "sufficient acknowledgement" which means identifying the work, by its title or other description, and the author of the work. To acknowledge the author of a film you may need to identify all of the following: the author of the screenplay, the author of any underlying work such as a novel, the principal director, the production company and the author of the music. Other copyright works included in a film, such as songs or artwork, will also need to be acknowledged where relevant.</p>
+                    <p><strong>(c) Fair dealing for the purpose of criticism or review (Section 30)</strong></p>
+                    <p>The use of the content must be no more than required by the specific purpose for which it is being used. A sufficient acknowledgement is required, as with the previous exception.<br><br>Please note that it is your sole responsibility to determine whether your proposed use of any Resource falls within one of the above or any other copyright exception. The notes set out above on the terms of the exceptions are not legal advice and are provided for your guidance only.<br><br>Use of the Resources for any other purpose must be specifically approved in writing by Into Film.</p>
+                </div>
             </div>
         </div>
     </div>
